7ac31a4a05372414dd129d047df8d27b66d73f55
tbmalaquias <tbmalaquia@gmail.com>
2024-09-28 15:07:29 -0300

    git commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What´s your grade percentage?");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string letter = "";
+        
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >=80)
+        {
+            letter = "B";
+        }
+        else if (percentage >=70)
+        {
+            letter = "C";
+        }
+        else if (percentage >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You are aproved! Congratulation");
+        }
+        else
+        {
+            Console.WriteLine("Sorry. Try again!");
+        }
     }
 }
