2ed46f4e289ff8db82a863a78a0c3a20bfaf1a90
tbmalaquias <tbmalaquia@gmail.com>
2024-09-28 15:35:06 -0300

    git commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Type numbers. Type 0 to stop.");
+
+        do
+        {
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+        // Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine("Sum: " + sum);
+
+        // Compute the average
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine("Average: " + average);
+
+        // Find the maximum
+        int maximum = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+        Console.WriteLine("Maximum: " + maximum);
     }
 }
