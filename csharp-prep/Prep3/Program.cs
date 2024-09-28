18b42996093b0bb882c8e24c6433e6047b07aff1
tbmalaquias <tbmalaquia@gmail.com>
2024-09-28 15:20:37 -0300

    git commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); // Generate a random number from 1 to 100
+
+        Console.WriteLine("Welcome to the Guessing Game!");
+
+        while (true)
+        {
+            Console.Write("Enter your guess (1-100): ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number.");
+                break; // Exit the loop if the guess is correct
+            }
+        }
     }
+    
 }
